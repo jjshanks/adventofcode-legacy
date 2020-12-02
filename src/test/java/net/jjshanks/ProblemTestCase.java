bc5af26a039efcfa38b8ebae4cc7d5008ce0e4b3
Joshua Shanks <jjshanks@gmail.com>
2020-12-02 10:41:35 -0800

    Simplify tests

---
+++ src/test/java/net/jjshanks/ProblemTestCase.java
@@ -0,0 +1,38 @@
+package net.jjshanks;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.After;
+import org.junit.Before;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+
+abstract public class ProblemTestCase {
+
+    private ListAppender<ILoggingEvent> appender;
+    private Logger solutionLogger = (Logger) LoggerFactory.getLogger(AbstractProblem.class);
+
+    @Before
+    public void problemSetup() {
+      appender = new ListAppender<>();
+      appender.start();
+      solutionLogger.addAppender(appender);
+    }
+
+    @After
+    public void problemTearDown() {
+      solutionLogger.detachAppender(appender);
+    }
+
+    protected void assertSolution(String expected) {
+        List<String> logs = appender.list.stream().map(ILoggingEvent::getFormattedMessage).collect(Collectors.toList());
+        assertTrue("Didn't find expected solution log line.", logs.contains(expected));
+    }
+
+}
--- src/test/java/net/jjshanks/day1/Day1Part2Test.java
+++ src/test/java/net/jjshanks/day1/Day1Part2Test.java
@@ -1,41 +1,18 @@
 package net.jjshanks.day1;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import org.junit.Test;
 
-import org.junit.After;
-import org.junit.Before;
-import org.slf4j.LoggerFactory;
-
-import ch.qos.logback.classic.Logger;
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.read.ListAppender;
-import junit.framework.TestCase;
 import net.jjshanks.AbstractProblem;
+import net.jjshanks.ProblemTestCase;
 import net.jjshanks.error.JollyException;
 
-public class Day1Part2Test extends TestCase {
+public class Day1Part2Test extends ProblemTestCase {
 
-    private ListAppender<ILoggingEvent> appender;
-    private Logger solutionLogger = (Logger) LoggerFactory.getLogger(AbstractProblem.class);
-
-    @Before
-    public void setUp() {
-      appender = new ListAppender<>();
-      appender.start();
-      solutionLogger.addAppender(appender);
-    }
-
-    @After
-    public void tearDown() {
-      solutionLogger.detachAppender(appender);
-    }
-
+    @Test
     public void testSimple() throws JollyException {
         AbstractProblem solver = new Day1Part2("day1/simpleInput");
         solver.run();
-        List<String> logs = appender.list.stream().map(ILoggingEvent::getFormattedMessage).collect(Collectors.toList());
-        assertTrue("Didn't find expected solution log line.", logs.contains("The solution is: 12090"));
+        assertSolution("The solution is: 12090");
     }
 
 }
