cfe119a4a16f094ac3e507e6e120f20018c3565c
Joshua Shanks <jjshanks@gmail.com>
2020-12-04 09:38:04 -0800

    Switch to using a scanner for splitting input

--- src/main/java/net/jjshanks/InputReader.java
+++ src/main/java/net/jjshanks/InputReader.java
@@ -1,13 +1,10 @@
 package net.jjshanks;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -16,6 +13,8 @@ import net.jjshanks.error.JollyException;
 
 public class InputReader {
 
+    static final String DEFULAT_INPUT_DELIMITER = "[\r\n]+";
+
     private final String inputName;
 
     public InputReader(String inputName) {
@@ -23,20 +22,26 @@ public class InputReader {
     }
 
     public List<String> getInput() throws JollyException {
-        return getInput(Function.identity(), Collectors.toList());
+        return getInput(Function.identity(), Collectors.toList(), DEFULAT_INPUT_DELIMITER);
     }
 
     public <U> List<U> getInput(Function<String, U> mapper) throws JollyException {
-        return getInput(mapper, Collectors.toList());
+        return getInput(mapper, Collectors.toList(), DEFULAT_INPUT_DELIMITER);
     }
 
-    public <U> List<U> getInput(Function<String, U> mapper, Collector<U, ?, List<U>> collector) throws JollyException {
+    public <U> List<U> getInput(Function<String, U> mapper, Collector<U, ?, List<U>> collector, String inputDelimiter) throws JollyException {
         try {
-            URL url = getClass().getClassLoader().getResource(inputName);
-            Path path = Paths.get(url.toURI());
-            List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+            InputStream is = getClass().getClassLoader().getResourceAsStream(inputName);
+            Scanner scan = new Scanner(is);
+            scan.useDelimiter(inputDelimiter);
+            List<String> lines = new ArrayList<>();
+            while (scan.hasNext()) {
+                lines.add(scan.next().trim());
+            }
+            scan.close();
+            is.close();
             return lines.stream().map(mapper).collect(collector);
-        } catch (IOException|URISyntaxException e) {
+        } catch (IOException e) {
             throw new JollyException(e);
         }
     }
